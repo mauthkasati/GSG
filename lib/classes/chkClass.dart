52bf31e368f31c924363e8f305eb75f0a485b48c
mauthkasati <76538592+mauthkasati@users.noreply.github.com>
2022-10-10 00:16:22 +0300

    Delete lib directory

--- lib/classes/chkClass.dart
+++
@@ -1,9 +0,0 @@
-class ChkClass {
-  String? text = 'default';
-  bool? chk = false;
-  ChkClass(this.text, this.chk);
-  ChkClass.fromMap(Map<String, dynamic> mp) {
-    text = mp['text'];
-    chk = mp['val'];
-  }
-}
--- lib/data/data.dart
+++
@@ -1,8 +0,0 @@
-List<Map<String, dynamic>>? data = [
-  {'text': 'writing', 'val': true},
-  {'text': 'reading', 'val': true},
-  {'text': 'eating', 'val': true},
-  {'text': 'sleeping', 'val': true},
-  {'text': 'coding', 'val': true},
-  {'text': 'playing', 'val': true}
-];
